c150f35cec3a14c7bb23c2a004e5d43b0014f678
Sam Kauffman <samueljkauff@gmail.com>
2026-02-10 21:37:22 -0500

    Small

--- src-tauri/src/backend/watcher.rs
+++ src-tauri/src/backend/watcher.rs
@@ -1,12 +1,13 @@
 use std::{sync::mpsc::channel};
 use notify::{Config, RecommendedWatcher, Watcher};
+
 fn start() {
     let (tx, rx) = channel();
 
     let mut watcher = RecommendedWatcher::new(tx, Config.default()).unwrap();
 
     watcher.watch(Path::new("~/Downloads")).unwrap();
-
+    
     for res in rx {
         match res {
             Ok(_) => handle_change(),
@@ -16,5 +17,5 @@ fn start() {
 }
 
 fn handle_change() {
-    
+    println!("hello");
 }
