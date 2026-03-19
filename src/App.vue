26fcc2231179a1f46cb3a4d3598d87ff69a9756d
Sam Kauffman <samueljkauff@gmail.com>
2026-03-19 00:35:21 -0400

    Vue flow first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
   <div id="app">
-    <FolderScene :nodes="nodes" />
+    <FolderScene/>
   </div>
 </template>
 
 <script setup lang="ts">
 import FolderScene from './components/FolderScene.vue';
-import { invoke } from '@tauri-apps/api/core';
-import { homeDir } from '@tauri-apps/api/path';
-import { ref, onMounted } from "vue";
-import { FolderNode } from "./interfaces/FolderNode"
 
-invoke("start");
-
-const nodes = ref<FolderNode[]>([]);
-
-onMounted(async () => {
-  const rootPath = await homeDir();
-  nodes.value = await invoke<FolderNode[]>("load_children", { root: rootPath});
-});
 </script>
 
 <style>
--- src/components/FolderScene.vue
+++ src/components/FolderScene.vue
@@ -1,22 +1,38 @@
 <template>
-  <VueFlow :nodes="nodes"></VueFlow>
+  <VueFlow :nodes="nodes" :edges="edges"></VueFlow>
 </template>
 
 <script lang="ts">
+import { homeDir } from '@tauri-apps/api/path';
 import { VueFlow } from '@vue-flow/core';
-import { FolderNode } from "../interfaces/FolderNode";
+import type { Edge, Node } from '@vue-flow/core'
+import { FolderNode } from '../interfaces/FolderNode';
+import { invoke } from '@tauri-apps/api/core';
 
 export default {
   data() {
     return {
-      
+      edges: [] as Edge[],
+      nodes: [] as Node[],
     }
   },
-  props: {
-    nodes: {
-      type: Object,
-      required: true,
-    }
+  async mounted() {
+  const rootPath = await homeDir();
+  const homeNodes = await invoke<FolderNode[]>("load_children", { root: rootPath});
+  this.nodes = homeNodes.map((folder, index) => ({
+        id: folder.path,
+        position: {
+          x: index * 100,
+          y: 250
+        },
+        data: {
+          label: folder.name,
+          folder
+        }
+      }))
+    },
+    components: {
+    VueFlow
+  },
   }
-}
 </script>
