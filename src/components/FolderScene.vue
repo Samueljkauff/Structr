48e4aee2277db1be8db3a428e52c59efb43e7eb3
Sam Kauffman <samueljkauff@gmail.com>
2026-03-19 00:59:26 -0400

    Vue flow formatted

--- src/components/FolderScene.vue
+++ src/components/FolderScene.vue
@@ -3,36 +3,38 @@
 </template>
 
 <script lang="ts">
-import { homeDir } from '@tauri-apps/api/path';
-import { VueFlow } from '@vue-flow/core';
-import type { Edge, Node } from '@vue-flow/core'
-import { FolderNode } from '../interfaces/FolderNode';
-import { invoke } from '@tauri-apps/api/core';
+import { homeDir } from "@tauri-apps/api/path";
+import { VueFlow } from "@vue-flow/core";
+import type { Edge, Node } from "@vue-flow/core";
+import { FolderNode } from "../interfaces/FolderNode";
+import { invoke } from "@tauri-apps/api/core";
 
 export default {
   data() {
     return {
       edges: [] as Edge[],
       nodes: [] as Node[],
-    }
+    };
   },
   async mounted() {
-  const rootPath = await homeDir();
-  const homeNodes = await invoke<FolderNode[]>("load_children", { root: rootPath});
-  this.nodes = homeNodes.map((folder, index) => ({
-        id: folder.path,
-        position: {
-          x: index * 100,
-          y: 250
-        },
-        data: {
-          label: folder.name,
-          folder
-        }
-      }))
-    },
-    components: {
-    VueFlow
+    const rootPath = await homeDir();
+    const homeNodes = await invoke<FolderNode[]>("load_children", {
+      root: rootPath,
+    });
+    this.nodes = homeNodes.map((folder, index) => ({
+      id: folder.path,
+      position: {
+        x: index * 100,
+        y: 250,
+      },
+      data: {
+        label: folder.name,
+        folder,
+      },
+    }));
   },
-  }
+  components: {
+    VueFlow,
+  },
+};
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import './styles.css'
 
 import '@vue-flow/core/dist/style.css'
 
