818b88c31de8fe286d6218ceac1e3bcbb4809977
Sam Kauffman <samueljkauff@gmail.com>
2026-03-23 00:02:22 -0400

    Tree expands on click now

--- src/components/FolderScene.vue
+++ src/components/FolderScene.vue
@@ -1,5 +1,5 @@
 <template>
-  <VueFlow :nodes="nodes" :edges="edges" :nodes-draggable="false">
+  <VueFlow @node-click="handleNodeClick" :nodes="nodes" :edges="edges" :nodes-draggable="false">
     <MiniMap />
     <Background :variant="BackgroundVariant.Dots" />
     <Controls position="top-left" :show-fit-view="true" :show-interactive="false" />
@@ -9,7 +9,7 @@
 <script lang="ts">
 import { homeDir } from "@tauri-apps/api/path";
 import { useVueFlow, VueFlow } from "@vue-flow/core";
-import type { Edge, Node } from "@vue-flow/core";
+import type { Edge, Node, NodeMouseEvent } from "@vue-flow/core";
 import { FolderNode } from "../interfaces/FolderNode";
 import { invoke } from "@tauri-apps/api/core";
 import { Background, BackgroundVariant, MiniMap } from '@vue-flow/additional-components';
@@ -21,46 +21,58 @@ export default {
     return {
       edges: [] as Edge[],
       nodes: [] as Node[],
+      dialog: false,
       BackgroundVariant,
     };
   },
   async mounted() {
     const rootPath = await homeDir();
-    const homeNodes = await invoke<FolderNode[]>("load_children", {
-      root: rootPath,
-    });
 
     const homeNode: Node = {
       id: rootPath,
       position: { x: 0, y: 0 },
-      data: { label: "Home", folder: { path: rootPath, name: "Home" } },
+      data: { label: "Home", folder: { path: rootPath, name: "Home" }, layer: 1 },
     };
-
-    const childSpacing = 125;
-    const totalWidth = (homeNodes.length - 1) * childSpacing;
-    const startX = -totalWidth / 2;
-
-    const childNodes: Node[] = homeNodes.map((folder, index) => ({
-      id: folder.path,
-      position: {
-        x: startX + index * childSpacing,
-        y: 250,
-      },
-      data: { label: folder.name, folder },
-    }));
-
-    this.nodes = [homeNode, ...childNodes];
-
-    this.edges = childNodes.map((child) => ({
-      id: `${rootPath}-${child.id}`,
-      source: rootPath,
-      target: child.id,
-      type: "smoothstep",
-    }));
+    this.nodes = [homeNode];
   },
   setup() {
     useVueFlow();
     return {};
+  },
+  methods: {
+    async handleNodeClick({ node }: NodeMouseEvent) {
+      const path = node.id as string;
+      const layer = node.data.layer + 1;
+      const children = await invoke<FolderNode[]>("load_children", {root:path});
+
+      const childSpacing = 125;
+      const totalWidth = (children.length - 1) * childSpacing;
+      const startX = -totalWidth / 2;
+      const Y = layer*100;
+      if(children.length <= 20) {
+        const childNodes: Node[] = children.map((folder, index) => ({
+          id: folder.path,
+          position: {
+            x: startX + index * childSpacing,
+            y: Y,
+          },
+          data: { label: folder.name, folder, layer },
+        }));
+
+        const childEdges: Edge[] = childNodes.map((child) => ({
+          id: `${node.id}-${child.id}`,
+          source: node.id,
+          target: child.id,
+          type: "smoothstep",
+        }));
+
+      this.nodes = [...this.nodes, ...childNodes];
+      this.edges = [...this.edges, ...childEdges];
+      }
+
+      this.dialog = true;
+      //implement dialog logic
+    }
   },
   components: {
     VueFlow,
