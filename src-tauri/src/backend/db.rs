2f4c19698af1b82919207ab7d5eafea47c552893
Sam Kauffman <samueljkauff@gmail.com>
2026-04-06 14:36:34 -0400

    DB commands in place

--- src-tauri/src/backend/db.rs
+++ src-tauri/src/backend/db.rs
@@ -1,33 +1,56 @@
 use diesel::sqlite::SqliteConnection;
 use diesel::prelude::*;
-use crate::schema::folder_metadata;
+use chrono::Utc;
+use crate::schema::folder_metadata::dsl::*;
+use crate::domain::models::*;
 
 pub fn establish_connection() -> SqliteConnection {
     SqliteConnection::establish("metadata.db")
         .expect("Error connecting to SQLite database")
 }
 
-pub fn create_metadata() {
+pub fn upsert_metadata(
+    target_path: &str,
+    new_description: &str,
+) -> QueryResult<usize> {
     let mut conn = establish_connection();
 
+    diesel::insert_into(folder_metadata)
+        .values((
+            path.eq(target_path),
+            description.eq(new_description),
+            updated_at.eq(now()),
+        ))
+        .on_conflict(path)
+        .do_update()
+        .set((
+            description.eq(new_description),
+            updated_at.eq(now()),
+        ))
+        .execute(&mut conn)
 }
 
-pub fn update_metadata() {
+pub fn remove_metadata(target_path: &str) -> QueryResult<usize>{
     let mut conn = establish_connection();
 
+    diesel::delete(folder_metadata.filter(path.eq(target_path)))
+        .execute(&mut conn)
 }
 
-pub fn remove_metadata() {
+pub fn get_metadata(target_path: &str) -> QueryResult<NodeMetadata> {
     let mut conn = establish_connection();
 
+        folder_metadata
+        .filter(path.eq(target_path))
+        .first(&mut conn)
 }
 
-pub fn get_metadata() {
+pub fn get_all_metadata() -> QueryResult<Vec<NodeMetadata>> {
     let mut conn = establish_connection();
 
+    folder_metadata.load::<NodeMetadata>(&mut conn)
 }
 
-pub fn get_all_metadata() {
-    let mut conn = establish_connection();
-
+fn now() -> chrono::NaiveDateTime {
+    Utc::now().naive_utc()
 }
